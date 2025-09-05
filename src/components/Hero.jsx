0cacbec0f4eb66995b1937147e7eeca435fee7ef
psan3333 <psan3333@mail.ru>
2025-09-05 10:56:19 +0700

    Implement Hero Scroll Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 import { useGSAP } from "@gsap/react"
 import gsap from 'gsap';
 import { SplitText } from 'gsap/all';
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
@@ -36,6 +41,26 @@ const Hero = () => {
         })
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0);
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const videoTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            videoTimeline.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            });
+        };
+
     }, []);
 
     return (
@@ -70,6 +95,14 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="videos/output.mp4"
+                    muted playsInline preload="auto"
+                />
+            </div>
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         navTween.fromTo('nav', {
             backgroundColor: 'transparent'
         }, {
-            backgroundColor: "#000000050",
+            backgroundColor: "#00000050",
             backgroundFilter: 'blur(10px)',
             duration: 1,
             ease: 'power1.inOut'
